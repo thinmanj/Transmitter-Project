8f5930732bcb48ecb0d482220b5d645655bc1bbd
julio <thinmanj@gmail.com>
2025-01-21 00:39:30 -0400

    Using black on source code

--- transmitter.py
+++ transmitter.py
@@ -15,9 +15,9 @@ def server(args):
             while True:
                 conn, addr = server_s.accept()
                 logger.info(f"Connection from {addr}")
-                msg = conn.recv(1024).decode('utf-8')
+                msg = conn.recv(1024).decode("utf-8")
                 logger.debug(f"Client send: {msg}")
-                conn.send("ok".encode('utf-8'))
+                conn.send("ok".encode("utf-8"))
                 filename = msg
 
                 try:
@@ -32,7 +32,9 @@ def server(args):
                 except FileNotFoundError:
                     logger.exception(f"File {filename} or path not found")
                 except OSError:
-                    logger.exception(f"OS error ocurred while accessing file {filename}")
+                    logger.exception(
+                        f"OS error ocurred while accessing file {filename}"
+                    )
                 except Exception as err:
                     logger.exception("Unexpected error happened")
 
@@ -51,17 +53,19 @@ def client(args):
             try:
                 with open(args.filename, "rb") as in_file:
                     out_bound = f"{args.filename}"
-                    client_s.send(out_bound.encode('utf-8'))
-                    msg = client_s.recv(1024).decode('utf-8')
+                    client_s.send(out_bound.encode("utf-8"))
+                    msg = client_s.recv(1024).decode("utf-8")
                     logger.debug(f"Server answered: {msg}")
 
-                    for chunck in iter(lambda: in_file.read(1024), b''):
-                        client_s.sendall(chunck)                   
+                    for chunck in iter(lambda: in_file.read(1024), b""):
+                        client_s.sendall(chunck)
                         logger.info(f"Sending data of size {len(chunck)}")
             except FileNotFoundError:
                 logger.exception(f"File {args.filename} not found")
             except OSError:
-                logger.exception(f"OS Error happend while trying to access file {args.filename}")
+                logger.exception(
+                    f"OS Error happend while trying to access file {args.filename}"
+                )
             except Exception as err_c:
                 logger.exception("Unexpected error happened")
     except socket.error as err_c:
@@ -70,32 +74,39 @@ def client(args):
 
 def main():
 
-    logging.basicConfig(filename="transmitter.log", level=logging.DEBUG, format='%(asctime)s %(name)15s.%(funcName)s:%(lineno)s %(levelname)-8s %(message)s', datefmt='%m-%d %H:%M')
+    logging.basicConfig(
+        filename="transmitter.log",
+        level=logging.DEBUG,
+        format="%(asctime)s %(name)15s.%(funcName)s:%(lineno)s %(levelname)-8s %(message)s",
+        datefmt="%m-%d %H:%M",
+    )
 
     console = logging.StreamHandler()
     console.setLevel(logging.INFO)
 
-    formatter = logging.Formatter('%(name)12s.%(funcName)s:%(lineno)s %(levelname)-8s %(message)s')
+    formatter = logging.Formatter(
+        "%(name)12s.%(funcName)s:%(lineno)s %(levelname)-8s %(message)s"
+    )
     console.setFormatter(formatter)
-    logging.getLogger('').addHandler(console)
+    logging.getLogger("").addHandler(console)
 
-
-    parser = argparse.ArgumentParser(prog='trasmitter.py', usage='%(prog)s [options]')
-    parser.add_argument('action', choices=['send', 'recv'])
-    parser.add_argument('filename', nargs='?', default=None)
-    parser.add_argument('ip', nargs='?', default='127.0.0.1')
-    parser.add_argument('port', nargs='?', default=8088, type=int)
+    parser = argparse.ArgumentParser(prog="trasmitter.py", usage="%(prog)s [options]")
+    parser.add_argument("action", choices=["send", "recv"])
+    parser.add_argument("filename", nargs="?", default=None)
+    parser.add_argument("ip", nargs="?", default="127.0.0.1")
+    parser.add_argument("port", nargs="?", default=8088, type=int)
     args = parser.parse_args()
 
-    if args.action == 'send' and args.filename is None:
-        parser.error('send action requires a filename')
+    if args.action == "send" and args.filename is None:
+        parser.error("send action requires a filename")
 
-    if args.action == 'send':
+    if args.action == "send":
         client(args)
-    elif args.action == 'recv':
+    elif args.action == "recv":
         server(args)
     else:
         logger.error(f"oops, invalid action: {args.action}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
